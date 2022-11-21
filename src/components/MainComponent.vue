b7aeb303f7fce38a2a44e9ad4e46b177e8db06a6
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-11-21 17:43:50 +0100

    creating component

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <JumboSection />
+    <CompanySection />
+    <ServicesSection />
+    <ProcessSection />
+    <TeamSection />
+    <NewsSection />
+    <NewsletterSection />
+</template>
+
+<script>
+import JumboSection from './JumboSection.vue';
+import CompanySection from './CompanySection.vue';
+import ServicesSection from './ServicesSection.vue';
+import ProcessSection from './ProcessSection.vue';
+import TeamSection from './TeamSection.vue';
+import NewsSection from './NewsSection.vue';
+import NewsletterSection from './NewsletterSection.vue';
+
+
+
+
+
+
+
+
+export default {
+    components: { JumboSection, CompanySection, ServicesSection, ProcessSection, TeamSection, NewsSection, NewsletterSection }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
